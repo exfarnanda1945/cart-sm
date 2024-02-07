3d6f8dd7b1cb79748838911c895f64be65ea76b2
miko <exfar@samir.co.id>
2024-02-07 14:52:43 +0700

    product repo

---
+++ lib/model/product-repository.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:cart_sm/model/product.dart';
+import 'package:flutter/services.dart';
+
+class ProductRepository {
+  Future<List<Product>> fetchProducts() async {
+    String jsonString = await rootBundle.loadString('assets/products.json');
+    final jsonProduct = jsonDecode(jsonString) as List<Map<String, dynamic>>;
+
+    return jsonProduct.map((e) => Product.fromJson(e)).toList();
+  }
+}
