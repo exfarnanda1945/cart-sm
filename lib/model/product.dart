e8b1453da7013070308cbe2e9d488912a5c8ec25
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-02-08 21:29:58 +0700

    fix product fromJson

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -21,15 +21,25 @@ class Product extends Equatable {
   final double rate;
   final int count;
 
-  factory Product.fromJson(Map<String, dynamic> json) => Product(
-      id: json["id"] as int,
-      title: json["title"] as String,
-      price: json["price"] as double,
-      description: json["description"] as String,
-      category: json["category"] as String,
-      image: json["image"] as String,
-      rate: json["rate"] as double,
-      count: json["count"] as int);
+  factory Product.fromJson(Map<String, dynamic> json) {
+    final price = json["price"].runtimeType == int
+        ? (json["price"] as int).toDouble()
+        : json["price"];
+
+    final rate = json["rating"]["rate"].runtimeType == int
+        ? (json["rating"]["rate"] as int).toDouble()
+        : json["rating"]["rate"];
+
+    return Product(
+        id: json["id"] as int,
+        title: json["title"] as String,
+        price: price,
+        description: json["description"] as String,
+        category: json["category"] as String,
+        image: json["image"] as String,
+        rate: rate,
+        count: json["rating"]["count"] as int);
+  }
 
   @override
   List<Object?> get props => [id];
