56dc9b0f4b48c43577960323819618e13b48c34a
exfarnanda1945 <exfarnanda1945@gmail.com>
2024-02-14 14:12:57 +0700

    cart screen

--- lib/screen/cart/cart-screen.dart
+++ lib/screen/cart/cart-screen.dart
@@ -1,3 +1,4 @@
+import 'package:cart_sm/screen/cart/cart-card.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
@@ -8,9 +9,18 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xff3867d6),
+        iconTheme: const IconThemeData(
+          color: Colors.white, //change your color here
+        ),
         title: const Text(
           "Cart",
           style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [CartCard()],
         ),
       ),
     );
